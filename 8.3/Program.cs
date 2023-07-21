f37de6467fe360133832126474e7532090e912e3
Mihael Kardash <mixan193@gmail.com>
2023-07-22 01:25:04 +0300

    Добавьте файлы проекта.

---
+++ 8.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._3
+{
+    internal class Program
+    {
+        static HashSet<int> set = new HashSet<int>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                if (!set.Add(EnterNumber()))
+                {
+                    Console.WriteLine("Число уже существует");
+                }
+            }
+        }
+        static int EnterNumber()
+        {
+            while (true)
+            {
+                try
+                {
+                    return int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка ввода");
+                }
+            }
+        }
+    }
+}
